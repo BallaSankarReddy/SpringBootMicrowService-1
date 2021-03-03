d1b2ee71230c7fd9259357546cd960b073e69d84
BallaSankarReddy <ballasankarreddy@gmail.com>
2021-03-03 22:21:19 +0530

    first

---
+++ src/main/java/com/eurekaserver/registration/SpringBootMicrowService1Application.java
@@ -0,0 +1,13 @@
+package com.eurekaserver.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicrowService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicrowService1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurekaserver/registration/SpringBootMicrowService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurekaserver.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicrowService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
