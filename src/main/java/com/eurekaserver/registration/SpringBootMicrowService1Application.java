261bd6ca9c4a7d09fc3fca49ca79b2accb90c2eb
BallaSankarReddy <ballasankarreddy@gmail.com>
2021-03-03 22:28:29 +0530

    second commit

--- src/main/java/com/eurekaserver/registration/SpringBootMicrowService1Application.java
+++ src/main/java/com/eurekaserver/registration/SpringBootMicrowService1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMicrowService1Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringBootMicrowService1Application.class, args);
 	}
 
