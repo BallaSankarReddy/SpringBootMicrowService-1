e3200ec382efae5d602190286c5fc1d735b46d9b
BallaSankarReddy <ballasankarreddy@gmail.com>
2021-03-08 15:03:01 +0530

    calculation files

---
+++ src/main/java/com/eurekaserver/registration/EMILoanCalculation.java
@@ -0,0 +1,59 @@
+package com.eurekaserver.registration;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public interface EMILoanCalculation {
+
+	public static BigDecimal ONE_HUNDRED = new BigDecimal(100);
+	public static BigDecimal YEAR = new BigDecimal(12);
+
+	public static BigDecimal getPrincipalAmount(BigDecimal loanAmount, BigDecimal rateOfInterest, BigDecimal nofYear,
+			BigDecimal nofMonths) {
+		BigDecimal principalAmount = BigDecimal.ZERO;
+		if (null != nofYear) {
+
+			BigDecimal setScale = rateOfInterest.setScale(0, RoundingMode.FLOOR);
+			principalAmount = loanAmount.multiply(ONE_HUNDRED).divide(nofYear.multiply(setScale));
+
+		}
+
+		return principalAmount;
+
+	}
+
+	public static BigDecimal getTotalPrincipalAmount(BigDecimal loanAmount, BigDecimal totalRateOfInterest,
+			BigDecimal otherfee) {
+		BigDecimal principalAmount = BigDecimal.ZERO;
+		principalAmount = loanAmount.add(totalRateOfInterest).add(otherfee);
+
+		return principalAmount;
+
+	}
+
+	public static BigDecimal getTotalInterestAmount(BigDecimal loanAmount, BigDecimal rateOfInterest,
+			BigDecimal nofYear, BigDecimal nofMonths) {
+		BigDecimal totalInterestAmount = BigDecimal.ZERO;
+		if (null != nofMonths) {
+			BigDecimal multiply = loanAmount.multiply(nofMonths).multiply(rateOfInterest);
+			totalInterestAmount = multiply.divide(ONE_HUNDRED.multiply(YEAR));
+
+		}
+
+		return totalInterestAmount;
+
+	}
+
+	public static BigDecimal getEMIPaymentAmount(BigDecimal loanAmount, BigDecimal totalInterestAmount,
+			BigDecimal nofYear, BigDecimal nofMonths) {
+		BigDecimal monthlyPaymentAmount = BigDecimal.ZERO;
+		if (null != nofMonths) {
+			monthlyPaymentAmount = loanAmount.add(totalInterestAmount).divide(nofMonths);
+
+		}
+
+		return monthlyPaymentAmount;
+
+	}
+
+}
---
+++ src/main/java/com/eurekaserver/registration/FormarLoanCalculation.java
@@ -0,0 +1,67 @@
+package com.eurekaserver.registration;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public interface FormarLoanCalculation {
+
+	public static BigDecimal ONE_HUNDRED = new BigDecimal(100);
+	public static BigDecimal ONE_YEAR = new BigDecimal(12);
+	public static BigDecimal DEFAULT_DAYS = new BigDecimal(30);
+
+	public static BigDecimal getBrowWithInterestAmount(BigDecimal browAmount, BigDecimal rateOfInterest,
+			BigDecimal nofYear, BigDecimal nofMonths) {
+		BigDecimal totalBrowWithInterestAmount = BigDecimal.ZERO;
+		// formula :: PTR%100
+
+		if (null == nofYear) {
+			nofYear = ONE_YEAR;
+			totalBrowWithInterestAmount = PTRCaculation(browAmount, rateOfInterest, nofYear);
+		} else {
+			totalBrowWithInterestAmount = PTRCaculation(browAmount, rateOfInterest, nofYear);
+
+		}
+
+		return totalBrowWithInterestAmount;
+
+	}
+
+	public static BigDecimal getNoOfMonthsInterestAmount(BigDecimal browAmount, BigDecimal rateOfInterest,
+			BigDecimal nofYear, BigDecimal nofMonths) {
+		BigDecimal totalBrowWithInterestAmount = BigDecimal.ZERO;
+		BigDecimal monthily_Amount = BigDecimal.ZERO;
+		// formula :: PTR%100
+
+		if (null == nofYear) {
+			nofYear = ONE_YEAR;
+			totalBrowWithInterestAmount = PTRCaculation(browAmount, rateOfInterest, nofYear);
+		}
+
+		if (null != nofMonths) {
+
+			monthily_Amount = (totalBrowWithInterestAmount.divide(ONE_YEAR)).multiply(nofMonths);
+
+		}
+
+		return monthily_Amount;
+
+	}
+
+	public static BigDecimal getDailyInterestAmount(BigDecimal montilyInterstAmount) {
+		BigDecimal daily_interst_Amount = BigDecimal.ZERO;
+		if (montilyInterstAmount.compareTo(BigDecimal.ZERO) > 0) {
+			daily_interst_Amount = montilyInterstAmount.divide(DEFAULT_DAYS, 3, RoundingMode.HALF_UP);
+		}
+
+		return daily_interst_Amount;
+
+	}
+
+	public static BigDecimal PTRCaculation(BigDecimal browAmount, BigDecimal rateOfInterest, BigDecimal nofYear) {
+		BigDecimal totalBrowWithInterestAmount;
+		BigDecimal divide = (browAmount.divide(ONE_HUNDRED));
+		totalBrowWithInterestAmount = divide.multiply(nofYear).multiply(rateOfInterest);
+		return totalBrowWithInterestAmount;
+	}
+
+}
